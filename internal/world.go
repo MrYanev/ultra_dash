f336762f0fffb99f9a935b7afd3316eb1576b290
Atanas Yanev <atanasyanev2@gmail.com>
2023-09-27 12:54:01 +0100

    Setting up the rendering of the player

--- internal/world.go
+++ internal/world.go
@@ -1,7 +1,15 @@
 package internal
 
 import (
+	"log"
+
 	"github.com/bytearena/ecs"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	position   *ecs.Component
+	renderable *ecs.Component
 )
 
 func InitializeWorld() (*ecs.Manager, map[string]ecs.Tag) {
@@ -9,24 +17,38 @@ func InitializeWorld() (*ecs.Manager, map[string]ecs.Tag) {
 	manager := ecs.NewManager()
 
 	player := manager.NewComponent()
-	position := manager.NewComponent()
-	renderable := manager.NewComponent()
+	position = manager.NewComponent()
+	renderable = manager.NewComponent()
 	movable := manager.NewComponent()
+
+	//Adding the image for the player
+	playerImg, _, err := ebitenutil.NewImageFromFile("assets/player.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	//Creating a player entity which has all the structs that we
 	//created earlier, also we are asigning it with the corresponging
 	//tags
 	manager.NewEntity().
 		AddComponent(player, Player{}).
-		AddComponent(renderable, Renderable{}).
+		AddComponent(renderable, &Renderable{
+			Image: playerImg,
+		}).
 		AddComponent(movable, Movable{}).
 		AddComponent(position, &Position{
 			X: 40,
 			Y: 25,
 		})
 
+	//Adding a view for the player
 	players := ecs.BuildTag(player, position)
 	tags["players"] = players
+
+	//Adding a view for all rendable objects in the game
+	//And where each should be (NB!!!)
+	renderables := ecs.BuildTag(renderable, position)
+	tags["renderables"] = renderables
 
 	return manager, tags
 }
