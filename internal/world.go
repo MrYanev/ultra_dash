3d959a06d26c68f8136b67cd5a704fb6d1b9f9ee
Atanas Yanev <atanasyanev2@gmail.com>
2023-10-03 12:53:58 +0100

    Working on fighting system

--- internal/world.go
+++ internal/world.go
@@ -8,9 +8,13 @@ import (
 )
 
 var (
-	position   *ecs.Component
-	renderable *ecs.Component
-	monster    *ecs.Component
+	position    *ecs.Component
+	renderable  *ecs.Component
+	monster     *ecs.Component
+	health      *ecs.Component
+	meleeWeapon *ecs.Component
+	armor       *ecs.Component
+	name        *ecs.Component
 )
 
 func InitializeWorld(startingLevel Level) (*ecs.Manager, map[string]ecs.Tag) {
@@ -22,6 +26,10 @@ func InitializeWorld(startingLevel Level) (*ecs.Manager, map[string]ecs.Tag) {
 	position = manager.NewComponent()
 	renderable = manager.NewComponent()
 	movable := manager.NewComponent()
+	health = manager.NewComponent()
+	meleeWeapon = manager.NewComponent()
+	armor = manager.NewComponent()
+	name = manager.NewComponent()
 
 	//Adding the image for the player
 	playerImg, _, err := ebitenutil.NewImageFromFile("assets/player.png")
@@ -48,28 +56,59 @@ func InitializeWorld(startingLevel Level) (*ecs.Manager, map[string]ecs.Tag) {
 		AddComponent(position, &Position{
 			X: x,
 			Y: y,
-		})
+		}).
+		AddComponent(health, &Health{
+			MaxHealth:     30,
+			CurrentHealth: 30,
+		}).
+		AddComponent(meleeWeapon, &MeleeWeapon{
+			Name:          "Fist",
+			MinimumDamage: 1,
+			MaximumDamage: 3,
+			ToHitBonus:    2,
+		}).
+		AddComponent(armor, &Armor{
+			Name:       "Burlap Sack",
+			Defense:    1,
+			ArmorClass: 1,
+		}).
+		AddComponent(name, &Name{Label: "Player"})
 
 	//Add a Monster in each room except the player's room
 	for _, room := range startingLevel.Rooms {
 		if room.X1 != startingRoom.X1 {
 			mX, mY := room.Center()
 			manager.NewEntity().
-				AddComponent(monster, &Monster{
-					Name: "Skeleton",
-				}).
+				AddComponent(monster, &Monster{}).
 				AddComponent(renderable, &Renderable{
 					Image: monsterImg,
 				}).
 				AddComponent(position, &Position{
 					X: mX,
 					Y: mY,
-				})
+				}).
+				AddComponent(health, &Health{
+					MaxHealth:     10,
+					CurrentHealth: 10,
+				}).
+				AddComponent(meleeWeapon, &MeleeWeapon{
+					Name:          "Short Sword",
+					MinimumDamage: 2,
+					MaximumDamage: 6,
+					ToHitBonus:    0,
+				}).
+				AddComponent(armor, &Armor{
+					Name:       "Bone",
+					Defense:    3,
+					ArmorClass: 4,
+				}).
+				AddComponent(name, &Name{Label: "Skeleton"})
+
 		}
 	}
 
 	//Adding a view for the player
-	players := ecs.BuildTag(player, position)
+	players := ecs.BuildTag(player, position, health, meleeWeapon, armor, name)
 	tags["players"] = players
 
 	//Adding a view for all rendable objects in the game
@@ -78,7 +117,7 @@ func InitializeWorld(startingLevel Level) (*ecs.Manager, map[string]ecs.Tag) {
 	tags["renderables"] = renderables
 
 	//Adding a view for monsters for faster access
-	monsters := ecs.BuildTag(monster, position)
+	monsters := ecs.BuildTag(monster, position, health, meleeWeapon, armor, name)
 	tags["monsters"] = monsters
 
 	return manager, tags
